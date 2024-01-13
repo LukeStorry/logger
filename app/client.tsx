33ee18f24e917a2c609fc24d837630106187c725
Luke Storry <luke.storry@letsdothis.com>
2024-01-13 22:41:40 +0000

    enable multi-line input with responsive

--- app/client.tsx
+++ app/client.tsx
@@ -7,23 +7,23 @@ export function Form({ sheetTitles }: { sheetTitles: string[] }) {
 
   return (
     <form action={action}>
-      <input
+      <textarea
         required
-        type="text"
         name="log"
-        className="mb-10 h-8 w-full rounded p-2 "
+        className="mb-10 min-h-24 w-full rounded p-2 "
       />
-
       <input type="datetime-local" name="date" className=" mr-4 rounded p-2 " />
 
-      <select name="sheet" className="rounded p-2">
+      <select name="sheet" className=" rounded p-2">
         {sheetTitles.map((sheet) => (
           <option key={sheet}>{sheet}</option>
         ))}
       </select>
 
       <SubmitButton />
-      {result && <div className="mt-10 text-center text-white">{result}</div>}
+      {result && (
+        <div className="text-center text-white lg:inline">{result}</div>
+      )}
     </form>
   );
 }
@@ -35,7 +35,7 @@ function SubmitButton() {
       type="submit"
       disabled={pending}
       className={
-        "ml-8 w-20 rounded p-3 text-white " +
+        "m-8 w-20 rounded p-3 text-white" +
         (pending ? " bg-cyan-600 " : " bg-cyan-800 ")
       }
     >
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Form } from "./client";
 export default async function Page() {
   return (
     <main className="min-h-screen bg-slate-800 p-1 py-24 md:px-24">
-      <div className="mx-auto h-80 items-center rounded border border-white bg-cyan-500 p-1 py-10 md:px-10">
+      <div className="mx-auto items-center rounded border border-white bg-cyan-500 p-1 py-10 md:px-10">
         <Form sheetTitles={await getSheetTitles()} />
       </div>
     </main>
