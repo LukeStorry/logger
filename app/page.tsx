5854d0e44749019b6725c535bbefa7e5a6f7af43
Luke Storry <luke.storry@letsdothis.com>
2024-01-13 22:53:59 +0000

    add basic passkey

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,19 @@
 import { getSheetTitles } from "./server";
 import { Form } from "./client";
 
-export default async function Page() {
+export default async function Page({ searchParams }: { searchParams: any }) {
+  const searchKeys = Object.keys(searchParams);
+  const allowed =
+    searchKeys.length == 1 && searchKeys[0] === process.env.PASSKEY;
+
   return (
     <main className="min-h-screen bg-slate-800 p-1 py-24 md:px-24">
       <div className="mx-auto items-center rounded border border-white bg-cyan-500 p-1 py-10 md:px-10">
-        <Form sheetTitles={await getSheetTitles()} />
+        {allowed ? (
+          <Form sheetTitles={await getSheetTitles()} />
+        ) : (
+          "Invalid passkey"
+        )}
       </div>
     </main>
   );
