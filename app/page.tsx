6cbcff4a86249e5986876afc0ca40f0b5e99b277
Luke Storry <luke.storry@letsdothis.com>
2024-01-14 17:37:11 +0000

    debug searchparams again

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { checkPasskey, getCategories, getOutputUrl } from "./server";
 import { Form } from "./client";
 
-export default async function Page({ searchParams, params }: any) {
+export default async function Page({ searchParams }: any) {
   const passkey = Object.keys(searchParams)[0];
   const authorised = passkey ? await checkPasskey(passkey) : false;
   const categories = authorised ? await getCategories() : ["default"];
@@ -10,6 +10,7 @@ export default async function Page({ searchParams, params }: any) {
     <main className="min-h-screen bg-slate-800 p-1 py-24 md:px-24">
       <div className="mx-auto items-center rounded border border-white bg-cyan-500 p-1 py-10 md:px-10">
         <Form categories={categories} showSettings={authorised} />
+      {JSON.stringify({ searchParams, passkey })}
       </div>
 
       <a href={await getOutputUrl()} target="_blank">
